16536bdb9a63add9386cab17fb88a1135a2688dd
yongj.zhuang <yongj.zhuang@outlook.com>
2024-02-08 23:49:05 +0800

    Change caller length to 255

--- schema/schema.sql
+++ schema/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS logbot.error_log (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `node` VARCHAR(25) NOT NULL COMMENT 'node name',
   `app` VARCHAR(25) NOT NULL COMMENT 'app name',
-  `caller` varchar(50) NOT NULL COMMENT 'caller name',
+  `caller` varchar(255) NOT NULL COMMENT 'caller name',
   `trace_id` varchar(25) NOT NULL DEFAULT '' COMMENT 'trace id',
   `span_id` varchar(25) NOT NULL DEFAULT '' COMMENT 'trace id',
   `err_msg` TEXT COMMENT 'error msg',
